51b191ccc409d93f63a7813b645597490195dcb5
Olga <terenteva2002@yandex.ru>
2021-05-13 11:05:58 +0300

    first push

--- src/hash_table.cpp
+++ src/hash_table.cpp
@@ -17,27 +17,37 @@ namespace itis {
       throw std::logic_error("hash table load factor must be in range [0...1]");
     }
 
-    // Tip: allocate hash-table buckets
+    buckets_ = std::vector<Bucket>{};
+    buckets_.resize(capacity);
   }
 
   std::optional<std::string> HashTable::Search(int key) const {
-    // Tip: compute hash code (index) and use linear search
+    auto index = hash(key);
+    auto bucket = buckets_[index];
+    for (std::pair<int, std::string> pair: bucket) {
+        if (pair.first == key) {
+            return pair.second;
+        }
+    }
     return std::nullopt;
   }
 
   void HashTable::Put(int key, const std::string &value) {
-    // Tip 1: compute hash code (index) to determine which bucket to use
-    // Tip 2: consider the case when the key exists (read the docs in the header file)
-
-    if (static_cast<double>(num_keys_) / buckets_.size() >= load_factor_) {
-      // Tip 3: recompute hash codes (indices) for key-value pairs (create a new hash-table)
-      // Tip 4: use utils::hash(key, size) to compute new indices for key-value pairs
+    auto index = hash(key);
+    for (std:: pair<int, std::string> &pair: buckets_[index]) {
+        if (pair.first == key) {
+            pair.second = value;
+            return;
+        }
     }
+    buckets_[index].emplace_back(key, value);
+    num_keys_++;
+
+
   }
 
   std::optional<std::string> HashTable::Remove(int key) {
-    // Tip 1: compute hash code (index) to determine which bucket to use
-    // TIp 2: find the key-value pair to remove and make a copy of value to return
+
     return std::nullopt;
   }
 
